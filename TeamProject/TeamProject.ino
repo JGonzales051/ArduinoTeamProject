7ec51c0bbd83e9a87500713d410509b3b7ff704d
Jeremiah <02Jeremiah.Gonzales@gulllakecs.org>
2020-01-09 14:14:22 -0500

    Coding for Traffic Light Simulator

--- TeamProject/TeamProject.ino
+++ TeamProject/TeamProject.ino
@@ -1,13 +1,50 @@
-/* Programmer: Andrew Gowan Jeremiah Gonzales
- * Date:1/7/20
- *  
- */
+/* Programmer(s): Andrew Gowan Jeremiah Gonzales
+   Date:1/7/20
+   Program: Traffic Light Simulator
+*/
+
+// variables
+int GREEN = 2;
+int YELLOW = 3;
+int RED = 4;
+int DELAY_GREEN = 5000;
+int DELAY_YELLOW = 2000;
+int DELAY_RED = 5000;
 
 
-void setup() {
-
+void setup()
+{
+  pinMode(GREEN, OUTPUT);
+  pinMode(YELLOW, OUTPUT);
+  pinMode(RED, OUTPUT);
 }
 
 void loop() {
-   
+  green_light();
+  delay(DELAY_GREEN);
+  yellow_light();
+  delay(DELAY_YELLOW);
+  red_light();
+  delay(DELAY_RED);
+}
+
+void green_light()
+{
+  digitalWrite(GREEN, HIGH);
+  digitalWrite(YELLOW, LOW);
+  digitalWrite(RED, LOW);
+}
+
+void yellow_light()
+{
+  digitalWrite(GREEN, LOW);
+  digitalWrite(YELLOW, HIGH);
+  digitalWrite(RED, LOW);
+}
+
+void red_light()
+{
+  digitalWrite(GREEN, LOW);
+  digitalWrite(YELLOW, LOW);
+  digitalWrite(RED, HIGH);
 }
