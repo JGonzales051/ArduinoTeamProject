cabe716f9337c98a738caf305adfbc1e6e99427f
Andrew Gowan <andrew_847@icloud.com>
2020-01-28 11:13:57 -0800

    add all the coding for the temperature sensor

--- TeamProject/TeamProject.ino
+++ TeamProject/TeamProject.ino
@@ -3,48 +3,46 @@
    Program: Traffic Light Simulator
 */
 
-// variables
-int GREEN = 2;
-int YELLOW = 3;
-int RED = 4;
-int DELAY_GREEN = 5000;
-int DELAY_YELLOW = 2000;
-int DELAY_RED = 5000;
-
+#define sensorPin  A0
+#define greenLED 8
+#define yellowLED 9
+#define redLED 10
 
 void setup()
 {
-  pinMode(GREEN, OUTPUT);
-  pinMode(YELLOW, OUTPUT);
-  pinMode(RED, OUTPUT);
+  pinMode(greenLED, OUTPUT);
+  pinMode(yellowLED, OUTPUT);
+  pinMode(redLED, OUTPUT);
+  Serial.begin(9600);
 }
 
-void loop() {
-  green_light();
-  delay(DELAY_GREEN);
-  yellow_light();
-  delay(DELAY_YELLOW);
-  red_light();
-  delay(DELAY_RED);
-}
-
-void green_light()
+void loop()
 {
-  digitalWrite(GREEN, HIGH);
-  digitalWrite(YELLOW, LOW);
-  digitalWrite(RED, LOW);
-}
+  int reading = analogRead(sensorPin);
+  float voltage = reading * 5.0;
+  voltage /= 1024.0;
+  float temperatureC = (voltage - 0.5) * 100 ;
+  float temperatureF = (temperatureC * 9.0 / 5.0) + 32.0;
 
-void yellow_light()
-{
-  digitalWrite(GREEN, LOW);
-  digitalWrite(YELLOW, HIGH);
-  digitalWrite(RED, LOW);
-}
+  Serial.print(voltage); Serial.print(" volts  -  ");
+  Serial.print(temperatureC); Serial.print(" degrees C  -  ");
+  Serial.print(temperatureF); Serial.println(" degrees F");
 
-void red_light()
-{
-  digitalWrite(GREEN, LOW);
-  digitalWrite(YELLOW, LOW);
-  digitalWrite(RED, HIGH);
+  if (temperatureF >= 90) {
+    digitalWrite(greenLED, LOW);
+    digitalWrite(yellowLED, LOW);
+    digitalWrite(redLED, HIGH);
+  }
+  else if (temperatureF >= 80 && temperatureF < 90) {
+    digitalWrite(greenLED, LOW);
+    digitalWrite(yellowLED, HIGH);
+    digitalWrite(redLED, LOW);
+  }
+  else {
+    digitalWrite(greenLED, HIGH);
+    digitalWrite(yellowLED, LOW);
+    digitalWrite(redLED, LOW);
+  }
+
+  delay(100);
 }
