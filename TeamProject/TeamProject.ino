c834f6a59f91466e8934d89dfb978fef311ca414
Andrew Gowan <andrew_847@icloud.com>
2020-02-03 11:18:04 -0800

    Changed Temperature Thr
    Red High from 90 to 85

--- TeamProject/TeamProject.ino
+++ TeamProject/TeamProject.ino
@@ -28,12 +28,12 @@ void loop()
   Serial.print(temperatureC); Serial.print(" degrees C  -  ");
   Serial.print(temperatureF); Serial.println(" degrees F");
 
-  if (temperatureF >= 90) {
+  if (temperatureF >= 85) {
     digitalWrite(greenLED, LOW);
     digitalWrite(yellowLED, LOW);
     digitalWrite(redLED, HIGH);
   }
-  else if (temperatureF >= 80 && temperatureF < 90) {
+  else if (temperatureF >= 80 && temperatureF < 85) {
     digitalWrite(greenLED, LOW);
     digitalWrite(yellowLED, HIGH);
     digitalWrite(redLED, LOW);
